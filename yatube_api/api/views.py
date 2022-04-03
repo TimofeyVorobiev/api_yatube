b6a4a29d2c780aef1867cde743d00174dca0d360
Timofey Vorobiev <vorobiev.te@yandex.ru>
2022-04-03 10:00:08 +0300

    4

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,9 @@
+from posts.models import Post, Group, Comment
+
 from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
-from posts.models import Post, Group, Comment
 from rest_framework.permissions import IsAuthenticated
+
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostsSerializer, GroupsSerializer, CommentsSerializer
 
